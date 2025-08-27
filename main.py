ee335205ca6cefe51b674f8489b7b5dca180e6dd
Silakov <169160799+sibir9@users.noreply.github.com>
2025-08-27 22:47:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 import subprocess
-import os
 
 app = FastAPI()
 
 # Главная страница
 @app.get("/")
 async def root():
-    return FileResponse("index.html")
+    return FileResponse(
+        "index.html",
+        headers={
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
+        }
+    )
 
-# Webhook для GitHub — только POST
+# GitHub Webhook (только POST)
 @app.post("/webhook")
 async def webhook(request: Request):
     try:
