d1e090577b325c2220839fc441daa31f02994c3d
Silakov <169160799+sibir9@users.noreply.github.com>
2025-08-27 21:30:57 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /root/dex-cex-arbitrage || exit
+git reset --hard
+git pull origin main
+source ../venv/bin/activate
+pip install -r requirements.txt
+systemctl restart fastapi.service
+chmod +x deploy.sh
